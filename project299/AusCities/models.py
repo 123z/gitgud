028c6a28ac4064db115b49bb7c9e484626c87198
123z <jakecain619@hotmail.com>
2017-09-20 13:54:52 +1000

    Add files via upload

--- project299/AusCities/models.py
+++ project299/AusCities/models.py
@@ -1,31 +1,7 @@
 from django.db import models
 
-
-##class EnumField(models.Field):
-##    """
-##    A field class that maps to MySQL's ENUM type.
-##    From - https://stackoverflow.com/a/1530858
-##    Usage:
-##
-##    class Card(models.Model):
-##        suit = EnumField(values=('Clubs', 'Diamonds', 'Spades', 'Hearts'))
-##
-##    c = Card()
-##    c.suit = 'Clubs'
-##    c.save()
-##    """
-##    def __init__(self, *args, **kwargs):
-##        self.values = kwargs.pop('values')
-##        kwargs['choices'] = [(v, v) for v in self.values]
-##        kwargs['default'] = self.values[0]
-##        super(EnumField, self).__init__(*args, **kwargs)
-##
-##    def db_type(self, connection):
-##        return "enum({0})".format(','.join("'%s'" % v for v in self.values))
-
 class EnumField(models.Field):
 #From - https://stackoverflow.com/a/9633015
-    
     def __init__(self, *args, **kwargs):
         super(EnumField, self).__init__(*args, **kwargs)
         if not self.choices:
@@ -33,28 +9,51 @@ class EnumField(models.Field):
 
     def db_type(self, connection):
         return "enum(%s)" % ','.join("'%s'" % k for (k, _) in self.choices)
+    
 # Create your models here.
-
-
-
 class UserType(models.Model):
     userType = models.CharField(max_length=20)
     def __str__(self):
         return self.userType
     
 class User(models.Model):
-    userType = models.ForeignKey(UserType, on_delete=models.CASCADE, default = 1)
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=300)
-    email = models.EmailField(max_length=150)
-    def __str__(self):
-            return self.username
+    userid = models.AutoField(db_column='userID', primary_key=True)  # Field name made lowercase.
+    emailaddress = models.EmailField(db_column='emailAddress', max_length=80, blank=False, null=True)  # Field name made lowercase.
+    firstname = models.CharField(db_column='firstName', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    lastname = models.CharField(db_column='lastName', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    usertype = EnumField(choices=(('Student', 'Student'), ('Tourist', 'Tourist'), ('Businessman', 'Businessman')), default = 1)
+    password = models.CharField(max_length=400, blank=True, null=True)
+    class Meta:
+        #managed = False
+        db_table = 'user'
 
-    
-class User2(models.Model):
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    email = models.EmailField(max_length=150)
-    usertype = EnumField(choices=(('Student', 's'), ('Tourist', 't'), ('Businessman', 'b')))
-    def __str__(self):
-            return self.username
+
+class Location(models.Model):
+    locationid = models.AutoField(db_column='locationID', primary_key=True)  # Field name made lowercase.
+    name = models.CharField(max_length=60, blank=True, null=True)
+    address = models.CharField(max_length=80, blank=True, null=True)
+    city = models.CharField(max_length=45, blank=True, null=True)
+    phonenumber = models.CharField(db_column='phoneNumber', max_length=45, blank=True, null=True)  # Field name made lowercase.
+    emailaddress = models.CharField(db_column='emailAddress', max_length=80, blank=True, null=True)  # Field name made lowercase.
+    locationtype = models.CharField(db_column='locationType', max_length=10, blank=True, null=True)  # Field name made lowercase.
+    website = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'location'
+        
+class Collegedepartments(models.Model):
+    locationdepartmentid = models.ForeignKey('Location', models.DO_NOTHING, db_column='locationDepartmentID')  # Field name made lowercase.
+    department = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'collegedepartments'
+
+class Industrytypes(models.Model):
+    locationindustryid = models.ForeignKey('Location', models.DO_NOTHING, db_column='locationIndustryID')  # Field name made lowercase.
+    industrytype = models.CharField(db_column='industryType', max_length=45, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'industrytypes'
