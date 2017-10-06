bd70b003ac73fba1a56ae8db777bc6f310e205d8
phocheeky <30453126+phocheeky@users.noreply.github.com>
2017-10-06 21:37:57 +1000

    Create Insert_data_for_smart_city.sql

---
+++ Insert_data_for_smart_city.sql
@@ -0,0 +1,78 @@
+use smart_city_project;
+
+select * from location;
+
+/* Colleges added to database*/
+INSERT INTO location (name, address, city, PhoneNumber, emailAddress, locationType, website) 
+VALUES ('Queensland University Of Technology', '2 George Street, Brisbane City QLD 4000', 'Brisbane', 0731382000, 'askqut@qut.edu.au.', 'college', 'http://www.qut.edu.au/');
+
+INSERT INTO location (name, address, city, PhoneNumber, emailAddress, locationType, website)  
+VALUES ('UQ', 'The University of QueenslandSt Lucia QLD 4072', 'Brisbane', 0733651111, 'askus@library.uq.edu.au', 'college', 'http://www.uq.edu.au/');
+
+/* Libraries added to database*/
+INSERT INTO location (name, address, city, PhoneNumber, locationType, website) 
+VALUES ('Brisbane Square Liberary', '266 George Street, Brisbane City QLD 4000', 'Brisbane', 0724034166, 'Library', 'https://www.brisbane.qld.gov.au/facilities-recreation/libraries');
+
+INSERT INTO location (name, address, city, PhoneNumber, locationType, website)  
+VALUES ('West End Library', '178-180 Boundary Street, West End QLD 4101', 'Brisbane', 0734038620, 'Library', 'https://www.brisbane.qld.gov.au/facilities-recreation/libraries');
+
+/* hotels added to database*/
+
+INSERT INTO location (name, address, city, PhoneNumber, emailAddress, locationType, website)  
+VALUES ('ibis Styles Brisbane Elizabeth Street', '40 Elizabeth Street, Brisbane City QLD 4000', 'Brisbane', 0733379000, 'H8835@accor.com', 'hotel', 'https://www.accorhotels.com/gb/hotel-8835-ibis-styles-brisbane-elizabeth-street-/index.shtml');
+
+INSERT INTO location (name, address, city, PhoneNumber, emailAddress, locationType, website) 
+VALUES ('Gambaro Hotel', '33 Caxton Street, Petrie Terrace QLD 4000', 'Brisbane', 0733699500, 'reservations@gambarohotel.com.au', 'hotel', 'gambarohotel.com.au/');
+
+
+/* Industries added to database*/
+
+INSERT INTO location (name, address, city, PhoneNumber, locationType, website) 
+VALUES ('BOQ Private Banking', 'Level 6, 100 Skyring Terrace, Newstead QLD 4006', 'Brisbane', 1300764439, 'industry', 'http://www.boq.com.au/personal_privatebank_contact.htm');
+
+INSERT INTO location (name, address, city, PhoneNumber, locationType, website)  
+VALUES ('QLD Department of Health', '33 Charlotte Street, Brisbane City QLD 4000', 'Brisbane', 0732340111, 'industry', 'https://www.health.qld.gov.au/');
+
+
+/* Parks added to database*/
+
+INSERT INTO location (name, address, city, PhoneNumber, locationType, website) 
+VALUES ('City Botanic Gardens', '147 Alice  Street, Brisbane City QLD 4000', 'Brisbane', 0734038888 , 'park', 'https://www.brisbane.qld.gov.au/facilities-recreation/parks-venues/parks?');
+
+INSERT INTO location (name, address, city, PhoneNumber, locationType, website) 
+VALUES ('Roma Street Parklands', '1 Parkland Boulevard, Brisbane City QLD 4000', 'Brisbane', 0734038888 , 'park', 'https://www.brisbane.qld.gov.au/facilities-recreation/parks-venues/parks?');
+
+/* zoos added to database*/
+
+INSERT INTO location (name, address, city, PhoneNumber, emailAddress, locationType, website)  
+VALUES ('Lone Pine Koala Sanctuary', '708 Jesmond Road, Fig Tree Pocket QLD 4069', 'Brisbane', 0733781366, 'service@koala.net', 'zoo', 'https://koala.net/en-au/');
+
+INSERT INTO location (name, address, city, PhoneNumber, locationType, website) 
+VALUES ('Walkabout Creek Discovery Centre', '60 Mount Nebo Road, The Gap QLD 4061', 'Brisbane', 0731643600, 'zoo', 'https://www.npsr.qld.gov.au/parks/daguilar/walkabout-creek.htm');
+
+
+/* Museums added to database*/
+
+INSERT INTO location (name, address, city, PhoneNumber, emailAddress, locationType, website)  
+VALUES ('Queensland Art Gallery', 'Stanley Place Cultural Precinct, South Bank QLD 4101', 'Brisbane', 0738407303, 'gallery@qagoma.qld.gov.au', 'Museum', 'https://www.qagoma.qld.gov.au/');
+
+INSERT INTO location (name, address, city, PhoneNumber, emailAddress, locationType, website)  
+VALUES ('Museum of Brisbane', '64 Adelaide Street, Brisbane City QLD 4000', 'Brisbane', 0733390800, 'info@museumofbrisbane.com.au', 'Museum', 'http://www.museumofbrisbane.com.au/');
+
+/* Restaurants added to database*/
+
+INSERT INTO location (name, address, city, PhoneNumber, locationType, website) 
+VALUES ('Moda Restaurant', '12 Edward Street, Brisbane City QLD 4000', 'Brisbane', 0732217655,'Restaurant', 'http://www.modarestaurant.com.au/');
+
+
+INSERT INTO location (name, address, city, PhoneNumber, emailAddress, locationType, website)  
+VALUES ('Ecco Bistro', '100 Boundary Street, Brisbane City QLD 4000', 'Brisbane', 0738318344, 'mail@eccobistro.com', 'Restaurant', 'http://eccobistro.com.au/');
+
+
+/* Malls added to database*/
+
+INSERT INTO location (name, address, city, PhoneNumber, emailAddress, locationType, website) 
+VALUES ('MacArthur Central Shopping Centre', '255 Queen St, Brisbane City', 'Brisbane', 0730072300, 'info@macarthurcentral.com', 'mall', 'http://www.macarthurcentral.com');
+
+INSERT INTO location (name, address, city, PhoneNumber, locationType, website) 
+VALUES ('Wintergarden', '171-209 Queen Street Mall Brisbane QLD 4000', 'Brisbane', 0732299755, 'mall', 'http://wgarden.com.au/');
