935271bd8e446c2fc0afca801a05877afd975590
Jono <jonobailey01@gmail.com>
2017-10-31 18:22:48 +1000

    footer now stuck to bottom of above div

--- project299/AusCities/tests.py
+++ project299/AusCities/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from AusCities.models  import User, Location, Admin
 
-# Create your tests here.
+class LocationTests(TestCase):
+	
+	def setUp(self):
+		Location.objects.create(name="USQ", locationtype="College")
+		
+	def test(self):
+		ab = Location.objects.get(name="USQ")
+		
+		self.assertEqual(ab, "USQ")
