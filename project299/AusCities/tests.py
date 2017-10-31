0632de85eb6b3730d9fa8064774ea7d747002184
123z <jakecain619@hotmail.com>
2017-10-31 20:44:36 +1000

    Test cases
    Can now test with the ability to use the db

--- project299/AusCities/tests.py
+++ project299/AusCities/tests.py
@@ -10,3 +10,37 @@ class LocationTests(TestCase):
 		ab = Location.objects.get(name="USQ")
 		
 		self.assertEqual(ab, "USQ")
+
+class UserViewTests(TestCase):
+    def test_access_logged_in(self):
+        session = self.client.session
+        session['logged'] = 1
+        session.save()
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'logged in.')
+    def test_access_logged_out(self):
+        session = self.client.session
+        session['logged'] = 0
+        session.save()
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Remember me')
+    def test_admin_access_as_user(self):
+        session = self.client.session
+        session['logged'] = 1
+        session.save()
+        response = self.client.get('/admin/add_info/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'administrator')
+    def test_admin_access_as_admin(self):
+        session = self.client.session
+        session['logged'] = 2
+        session.save()
+        response = self.client.get('/admin/add_info/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Location name:')
+    def test_null_location(self):
+        Location.objects.create(name='QUT', locationtype='College')
+        response = self.client.get('/location/1/')
+        self.assertEqual(response.status_code, 500)
---
+++ project299/project299/tsettings.py
@@ -0,0 +1,5 @@
+from project299.settings import *
+DEBUG = False
+MIGRATION_MODULES = {
+    'AusCities': 'AusCities.test_migrations',
+}
